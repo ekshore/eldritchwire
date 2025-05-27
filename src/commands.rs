a0f8a14c798fedfa9b8351273c1efdeaefbd0a94
Caleb Ekstrand <ekstrand.caleb@ekshore.net>
2025-05-26 20:24:55 -0500

    Parse Command
    Adding parse_command() to commmand module

--- src/commands.rs
+++ src/commands.rs
@@ -6,3 +6,31 @@ use lens_commands::LensCommand;
 pub enum Command {
     Lens(LensCommand),
 }
+
+pub fn parse_command(cmd_category: usize, cmd_buffer: &[u8]) -> Command {
+    match cmd_category {
+        0x00 => Command::Lens(lens_commands::parse_lens_command(cmd_buffer)),
+        _ => todo!("Command category has either not been implemented or is invalid"),
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::{FixedPointDecimal, Operation, commands::LensCommand};
+
+    #[test]
+    fn parse_lens_focus_command() {
+        let command_data: [u8; 7] = [0x00, 0x80, 0x01, 0x9a, 0xfd, 0x00, 0x00];
+        let command = parse_command(0x00, &command_data);
+        assert_eq!(
+            command,
+            Command::Lens(LensCommand::Focus(
+                Operation::Increment,
+                FixedPointDecimal {
+                    raw_val: 0xFD9Au16 as i16
+                }
+            ))
+        )
+    }
+}
