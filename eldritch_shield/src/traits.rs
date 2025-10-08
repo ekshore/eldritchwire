8ce1acab8a73c468c64b9f0fe9ad7bf8cf6e1019
Caleb Ekstrand <ekstrand.caleb@ekshore.net>
2025-10-08 00:07:22 -0500

    removing the write_read method
    Because of the way that the shield works, every read is a write read.
    That is expected in the implementation

--- eldritch_shield/src/traits.rs
+++ eldritch_shield/src/traits.rs
@@ -3,14 +3,4 @@ pub trait I2cTransport {
 
     fn write(&mut self, addr: &[u8; 2], bytes: &[u8]) -> Result<(), Self::Error>;
     fn read(&mut self, addr: &[u8; 2], buffer: &mut [u8]) -> Result<(), Self::Error>;
-
-    fn write_read(
-        &mut self,
-        addr: &[u8; 2],
-        bytes: &[u8],
-        buffer: &mut [u8],
-    ) -> Result<(), Self::Error> {
-        self.write(addr, bytes)?;
-        self.read(addr, buffer)
-    }
 }
